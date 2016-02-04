fdc9a8bcf004027ed89fb647648b4908253c472a
Chyzwar <schizek.marcin@gmail.com>
2016-02-04 21:38:32 +0000

    rename and cleanup

--- src/MariaDB.js
+++ src/MariaDB.js
@@ -5,43 +5,43 @@ import fs from 'fs';
 
 
 export default class MariaDB {
-	constructor(config) {
-		this.config = config;
-		this.connection = new Sequelize(
-			config.database,
-			config.username,
-			config.password,
-			{
-				host: config.host,
-				dialect: 'mariadb',
+  constructor(config) {
+    this.config = config;
+    this.connection = new Sequelize(
+      config.database,
+      config.username,
+      config.password,
+      {
+        host: config.host,
+        dialect: 'mariadb',
 
-				pool :{
-					max: config.pool.max || 5,
-					min: config.pool.max || 0,
-					idle: config.pool.idle || 1000
-				}
-			});
-	}
+        pool:{
+          max: config.pool.max || 5,
+          min: config.pool.max || 0,
+          idle: config.pool.idle || 1000
+        }
+      });
+  }
 
-	/**
-	 * Publish DB Connection to global object
-	 * @param  {String} name
-	 */
-	publishGlobal(name='maria') {
-		global[name] = this.connection;
-	}
+  /**
+   * Publish DB Connection to global object
+   * @param  {String} name
+   */
+  publishGlobal(name='maria') {
+    global[name] = this.connection;
+  }
 
-	/**
-	 * Relative path to model dir
-	 * @param  {String} modelsDir
-	 */
-	importModels(modelsDir){
-		let mariaDir = path.join(process.cwd(), './src/server/storage');
-		let absoluteDir = path.join(mariaDir, modelsDir);
-
-		fs.readdirSync(absoluteDir)
-			.forEach((file) => {
-				this.connection.import(path.join(absoluteDir, file));
-			});
-	}
+  /**
+   * Relative path to model dir
+   * @param  {String} modelsDir
+   */
+  importModels(modelsDir){
+    let srcPath = path.join(process.cwd(), './src');
+    let searchDir = path.join(srcPath, modelsDir);
+    console.log(searchDir);
+    fs.readdirSync(searchDir)
+      .forEach((file) => {
+        this.connection.import(path.join(searchDir, file));
+      });
+  }
 }
