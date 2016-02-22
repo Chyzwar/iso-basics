241d76066ed84adabf6d3d410c9c2c037e81b6c8
Chyzwar <schizek.marcin@gmail.com>
2016-02-22 02:48:04 +0000

    better bookshelf

--- src/BookshelfDB.js
+++ src/BookshelfDB.js
@@ -3,29 +3,7 @@ import bookshelf from 'bookshelf';
 
 class BookshelfDB {
   constructor(config) {
-    const { host, user, password, database } = config.connection;
-    const { charset = 'utf8' } = config.connection;
-    const { min = 2, max = 10 } = config.pool;
-    const { tableName } = config.migrations;
-
-    this.knex = knex({
-      client: config.client,
-      connection: {
-        host,
-        user,
-        password,
-        database,
-        charset,
-      },
-      pool: {
-        min,
-        max,
-      },
-      migrations: {
-        tableName,
-      },
-    });
-
+    this.knex = knex(config);
     this.bookshelf = bookshelf(this.knex);
   }
 
