ded09b0c07f3003585d96b27cc500aa60b26b826
Chyzwar <schizek.marcin@gmail.com>
2016-02-25 08:25:50 +0000

    work on front

--- src/ReactClient.js
+++ src/ReactClient.js
@@ -1,7 +1,32 @@
+import React from 'react'
+import ReactDOM from 'react-dom'
+
+import { Provider } from 'react-redux'
+import { syncHistoryWithStore } from 'react-router-redux';
+import { Router, Route, browserHistory } from 'react-router'
+
 
 class ReactClient{
-  constructor() {
+  constructor(register) {
+    this.routes = register.routes;
+    this.store = register.store;
+  }
 
+  init(){
+    const initState = window.initState;
+    const history = syncHistoryWithStore(browserHistory, this.store);
+
+    ReactDOM.render(
+      <Provider store={this.store}>
+        <Router history={history}>
+          <Route path="/" component={App}>
+            <Route path="foo" component={Foo}/>
+            <Route path="bar" component={Bar}/>
+          </Route>
+        </Router>
+      </Provider>,
+      document.getElementById('mount')
+    )
   }
 }
 
